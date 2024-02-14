c45b44968c509210a41331bb12c2ade60895e42c
Mileto Di Marco <mileto.di.marco@howest.be>
2024-02-14 15:09:50 +0100

    Add project files.

---
+++ Pri.CleanArchitecture.Music.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pri.CleanArchitecture.Music.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.CleanArchitecture.Music.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Record> Records { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Property> Properties { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
