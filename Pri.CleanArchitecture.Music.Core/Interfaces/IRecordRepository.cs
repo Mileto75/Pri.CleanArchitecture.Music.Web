f89f6db91db832255bf0fcab6242f9b7e317396c
Mileto Di Marco <mileto.di.marco@howest.be>
2024-02-14 16:28:28 +0100

    Add repository class for record

---
+++ Pri.CleanArchitecture.Music.Core/Interfaces/IRecordRepository.cs
@@ -0,0 +1,18 @@
+﻿using Pri.CleanArchitecture.Music.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.CleanArchitecture.Music.Core.Interfaces
+{
+    public interface IRecordRepository
+    {
+        //CRUD
+        Task<IEnumerable<Record>> GetAllAsync();
+        Task<Record> GetByIdAsync(int id);
+        IQueryable<Record> GetAll();
+        Task<Record> AddAsync(Record toRecord);
+    }
+}
---
+++ Pri.CleanArchitecture.Music.Infrastructure/Repositories/RecordRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pri.CleanArchitecture.Music.Core.Entities;
+using Pri.CleanArchitecture.Music.Core.Interfaces;
+using Pri.CleanArchitecture.Music.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.CleanArchitecture.Music.Infrastructure.Repositories
+{
+    public class RecordRepository : IRecordRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public RecordRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public Task<Record> AddAsync(Record toRecord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Record> GetAll()
+        {
+            return _applicationDbContext
+                .Records.AsQueryable();
+        }
+
+        public async Task<IEnumerable<Record>> GetAllAsync()
+        {
+            return await _applicationDbContext.Records.ToListAsync();
+        }
+
+        public Task<Record> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
